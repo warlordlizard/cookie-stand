a45bce2df809cf26233932f70aeff770d82b7625
warlordlizard <alexjbowers88@gmail.com>
2017-12-11 14:15:58 -0800

    made files and created store objects

---
+++ sales.js
@@ -0,0 +1,29 @@
+var pike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3
+}
+
+var airport = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2
+}
+
+var center = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7
+}
+
+var capHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3
+}
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6
+}
