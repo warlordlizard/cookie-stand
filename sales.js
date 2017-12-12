e0bf18d45c8853fa73a85e3741afc5eda518d5de
warlordlizard <alexjbowers88@gmail.com>
2017-12-12 15:56:50 -0800

    table made no hr titles

--- sales.js
+++ sales.js
@@ -9,6 +9,7 @@ function random(min,max) {
 //day 2 constructor
 
 var allStores = [];
+var storeTable = document.getElementById('store-sales');
 
 function Store(name, minCust, maxCust, avgCookie) {
   this.name= name;
@@ -18,25 +19,53 @@ function Store(name, minCust, maxCust, avgCookie) {
   this.custEachHr= [];
   this.cookiesEachHr= [];
   this.totalDailySales= 0;
-  this.calcCustEachHr= function(){
-    for(var i = 0; i < hours.length; i++) {
-      this.custEachHr.push(random(this.minCust, this.maxCust));
-    }
-  };
-  this.calcCookiesEachHr= function(){
-    this.calcCustEachHr();
-    for(var i = 0; i < hours.length; i++) {
-      var oneHour = Math.ceil(this.custEachHr[i] * this.avgCookie);
-      this.cookiesEachHr.push(oneHour);
-      this.totalDailySales += oneHour;
-    };
-  };
-
   this.calcCookiesEachHr();
   allStores.push(this);
+}
+
+
+Store.prototype.calcCustEachHr= function(){
+  for(var i = 0; i < hours.length; i++) {
+    this.custEachHr.push(random(this.minCust, this.maxCust));
+  }
 };
+
+Store.prototype.calcCookiesEachHr= function(){
+  this.calcCustEachHr();
+  for(var i = 0; i < hours.length; i++) {
+    var oneHour = Math.ceil(this.custEachHr[i] * this.avgCookie);
+    this.cookiesEachHr.push(oneHour);
+    this.totalDailySales += oneHour;
+  };
+};
+
+
+Store.prototype.render = function() {
+   var trEl = document.createElement('tr');
+   var tdEl = document.createElement('td');
+   tdEl.textContent = this.name;
+   trEl.appendChild(tdEl);
+
+   for (var i = 0; i < this.cookiesEachHr.length; i++) {
+     tdEl = document.createElement('td');
+     tdEl.textContent = this.cookiesEachHr[i];
+     trEl.appendChild(tdEl);
+   };
+   storeTable.appendChild(trEl);
+};
+
 new Store('1st and Pike', 23, 65, 6.3);
 new Store('Seatac International Airport', 3, 24, 1.2);
 new Store('Seattle Center', 11, 38, 3.7);
 new Store('Capitol Hill', 20, 38, 2.3);
 new Store('Alki Beach', 2, 16, 4.6);
+
+
+
+function renderStoreRows(){
+  for(var i = 0; i < allStores.length; i++){
+    allStores[i].render();
+  };
+}
+
+renderStoreRows();
