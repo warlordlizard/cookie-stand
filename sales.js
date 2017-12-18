d02b6a1d3418d2c5f5124bc9776ee0063635f03d
warlordlizard <alexjbowers88@gmail.com>
2017-12-17 16:13:16 -0800

    got form and table to work

--- sales.js
+++ sales.js
@@ -7,7 +7,11 @@ function random(min,max) {
 }
 
 //day 2 constructor
-
+var allStoresData = [ ['1st and Pike', 23, 65, 6.3],
+                       ['Seatac International Airport', 3, 24, 1.2],
+                       ['Seattle Center', 11, 38, 3.7],
+                       ['Capitol Hill', 20, 38, 2.3],
+                       ['Alki Beach', 2, 16, 4.6] ];
 var allStores = [];
 var form = document.getElementById('store_form');
 var storeTable = document.getElementById('store-sales');
@@ -24,26 +28,52 @@ function Store(name, minCust, maxCust, avgCookie) {
   this.custEachHr= [];
   this.cookiesEachHr= [];
   this.totalDailySales= 0;
-  this.calcCookiesEachHr();
-  allStores.push(this);
+  this.totalCust = 0;
+  this.totalCookies = 0;
+  // this.calcCookiesEachHr();
+  // allStores.push(this);
   // hourlytotal.push(this.totalDailySales);
+  this.fakeCust = function() {
+    for (var i =0; i<hours.length; i++) {
+      this.custEachHr[i] = random(this.minCust, this.maxCust);
+      this.cookiesEachHr[i] = Math.ceil(this.custEachHr[i] * this.avgCookie);
+      this.totalCust += this.custEachHr[i];
+      this.totalCookies += this.cookiesEachHr[i];
+    }
+  }
+  this.fakeCust();
 }
 
+for (var i = 0; i < allStoresData.length; i++) {
+  allStores.push(new Store(allStoresData[i][0], allStoresData[i][1], allStoresData[i][2], allStoresData[i][3]));
+}
 
-Store.prototype.calcCustEachHr= function(){
-  for(var i = 0; i < hours.length; i++) {
-    this.custEachHr.push(random(this.minCust, this.maxCust));
-  }
-};
-
-Store.prototype.calcCookiesEachHr= function(){
-  this.calcCustEachHr();
-  for(var i = 0; i < hours.length; i++) {
-    var oneHour = Math.ceil(this.custEachHr[i] * this.avgCookie);
-    this.cookiesEachHr.push(oneHour);
-    this.totalDailySales += oneHour;
-  };
-};
+// var totals = function() {
+//   var colTotals = [];
+//   var dailyTotal = 0;
+//   for (var i = 0; i < hours.length; i++) {
+//     var totalPerHr = 0;
+//     for (var j = 0; j < allStores.length; j++) {
+//       totalPerHr += allStores
+//     }
+//   }
+//
+// }
+//
+// Store.prototype.calcCustEachHr= function(){
+//   for(var i = 0; i < hours.length; i++) {
+//     this.custEachHr.push(random(this.minCust, this.maxCust));
+//   }
+// };
+// Store.prototype.calcCookiesEachHr= function(){
+//   this.calcCustEachHr();
+//   for(var i = 0; i < hours.length; i++) {
+//     var oneHour = Math.ceil(this.custEachHr[i] * this.avgCookie);
+//     this.cookiesEachHr.push(oneHour);
+//     this.totalDailySales += oneHour;
+//   };
+//
+// };
 
 // Store.prototype.calcHourlyTotal = function () {
 //   for(var i = 0; i < hours.length; i++) {
@@ -67,23 +97,27 @@ Store.prototype.render = function() {
    };
    storeTable.appendChild(trEl);
 };
+//
+// new Store('1st and Pike', 23, 65, 6.3);
+// new Store('Seatac International Airport', 3, 24, 1.2);
+// new Store('Seattle Center', 11, 38, 3.7);
+// new Store('Capitol Hill', 20, 38, 2.3);
+// new Store('Alki Beach', 2, 16, 4.6);
 
-new Store('1st and Pike', 23, 65, 6.3);
-new Store('Seatac International Airport', 3, 24, 1.2);
-new Store('Seattle Center', 11, 38, 3.7);
-new Store('Capitol Hill', 20, 38, 2.3);
-new Store('Alki Beach', 2, 16, 4.6);
+function unRender() {
+  document.getElementById('store-sales').innerText = '';
 
+}
 function formData(event) {
   event.preventDefault();
-  var reset = document.getElementById('store-sales');
-  reset.innerText = '';
+
   var name = event.target.store.value;
-  var minCust = event.target.minCust.value;
-  var maxCust = event.target.maxCust.value;
-  var avgCookie = event.target.avgCookie.value;
+  var minCust = parseInt(event.target.minCust.value);
+  var maxCust = parseInt(event.target.maxCust.value);
+  var avgCookie = parseInt(event.target.avgCookie.value);
 
   allStores.push(new Store(name, minCust, maxCust, avgCookie));
+  unRender();
   renderHeaderRow();
   // renderStoreRows();
   renderFormRow();
